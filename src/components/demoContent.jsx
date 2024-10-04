e45ad826d72d40fd25a19db38760a6713dd3e219
josearcos2023 <jose.arcos@tecsup.edu.pe>
2024-10-04 06:40:28 -0500

    3 botones de prueba habilitados Formato de cuadro de texto habilitado

---
+++ src/components/demoContent.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+const DemoContent = ({ generateResponse, question }) => {
+    const [newQuestion, setNewQuestion] = useState('');
+
+    const handleButtonClick = () => {
+        setNewQuestion(question);
+        generateResponse(newQuestion,setNewQuestion);
+    };
+
+    return (
+        
+        <div>
+            
+            <button 
+                className='btn-demo-content'
+                onClick={handleButtonClick}
+            >
+                {question}
+            </button>
+            
+        </div>
+    )
+}
+
+export default DemoContent
